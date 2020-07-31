514e59d2ae700047d974da7caf7e00d24fda654a
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-31 18:50:54 +0200

    Added custom parameters to the noise generator

--- Random_Map_Generator/Assets/Map_Generator.cs
+++ Random_Map_Generator/Assets/Map_Generator.cs
@@ -8,14 +8,45 @@ public class Map_Generator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap() {
-        float[,] noiseMap = Noise_Generator.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise_Generator.GenerateNoiseMap(mapWidth, mapHeight, seed , noiseScale, octaves, persistance, lacunarity, offset);
 
 
         Map_Display display = FindObjectOfType<Map_Display>();
         display.DrawNoiseMap(noiseMap);
     }
 
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+
+        if (lacunarity < 1) {
+            lacunarity = 1;
+        }
+
+        if (octaves < 0) {
+            octaves = 0;
+        }
+
+        
+
+    }
 }
