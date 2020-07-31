b1badd6bb40895ec769549d6afff564b61353037
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-07-31 18:05:57 +0200

    Perlin noise generator on 2d surface

---
+++ Random_Map_Generator/Assets/Map_Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Generator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise_Generator.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+
+        Map_Display display = FindObjectOfType<Map_Display>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+}
---
+++ Random_Map_Generator/Assets/Map_Generator_Editor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Map_Generator))]
+public class Map_Generator_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Map_Generator mapGen = (Map_Generator)target;
+
+        if (DrawDefaultInspector()) {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate")) {
+            mapGen.GenerateMap();
+        }
+    }
+}
